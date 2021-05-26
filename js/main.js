bcaf12a8cfca6d938c73bb7101fcb9e90f2b2a32
Eric Howey <eric.howey5@gmail.com>
2021-05-25 19:37:48 -0500

    Created azure function api

--- js/main.js
+++ js/main.js
@@ -2,13 +2,13 @@ window.addEventListener('DOMContentLoaded', (event) =>{
     getVisitCount
 })
 
-const functionApi = '';
+const functionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
     fetch(functionApi).then(response => {
-        return response.json()
-    }).then(response =>{
+        return response.json();
+    }).then(response => {
         console.log("Website called function API.");
         count = response.count;
         document.getElementById("counter").innertext = count;
